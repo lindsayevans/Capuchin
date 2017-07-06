cc95e79ababfaaf692c18c3a1c5946543d9ea8b3
Lindsay Evans <lindsay.evans@boomworks.com.au>
2017-07-06 17:21:38 +1000

    Implement CSS methods #2 * addClass * removeClass * toggleClass * hasClass

--- src/capuchin-collection.ts
+++ src/capuchin-collection.ts
@@ -21,4 +21,59 @@ export class CapuchinCollection<T extends Array<HTMLElement | Document>> extends
         }
     }
 
+    /**
+     * Checks if any element in the collection has the CSS class `name`
+     */
+    public hasClass(name: string): boolean {
+
+        let hasClass = false;
+
+        this.forEach(element => {
+            if (element.classList.contains(name)) {
+                hasClass = true;
+            }
+        });
+
+        return hasClass;
+    }
+
+    /**
+     * Adds CSS class `name` to all elements in collection
+     */
+    public addClass(name: string)
+        : CapuchinCollection<T> {
+
+        this.forEach(element => {
+            element.classList.add(name);
+        });
+
+        return this;
+    }
+
+    /**
+     * Removes CSS class `name` from all elements in collection
+     */
+    public removeClass(name: string)
+        : CapuchinCollection<T> {
+
+        this.forEach(element => {
+            element.classList.remove(name);
+        });
+
+        return this;
+    }
+
+    /**
+     * Toggles the CSS class `name` on all elements in collection
+     */
+    public toggleClass(name: string)
+        : CapuchinCollection<T> {
+
+        this.forEach(element => {
+            element.classList.toggle(name);
+        });
+
+        return this;
+    }
+
 }
--- test/index.ts
+++ test/index.ts
@@ -3,13 +3,6 @@ const fs = require('fs');
 import 'jest';
 import { $, Capuchin } from '../src/';
 
-const fixtureNames = [
-    'simple',
-    'complex'
-];
-
-let fixtures: {[id: string]: Document} = {};
-
 beforeEach(() => {
     // Load fixture HTML
     document.body.innerHTML = fs.readFileSync(`./test/fixtures/simple.html`);
@@ -25,7 +18,45 @@ describe('Capuchin should...', () => {
 
   test('return correct number of elements', () => {
 
-    expect($('.foo', fixtures.simple).length).toBe(5);
+    expect($('.foo').length).toBe(5);
+
+  });
+
+  test('check if a collection has an element with a class', () => {
+
+    expect($('.foo').hasClass('bar')).toBeTruthy();
+
+  });
+
+  test('check if a collection doesn\'t have an element with a class', () => {
+
+    expect($('.foo').hasClass('nope')).toBeFalsy();
+
+  });
+
+  test('add a class to elements', () => {
+
+    $('.foo').addClass('bar');
+
+    expect(document.querySelector('.foo').classList.contains('bar')).toBeTruthy();
+
+  });
+
+  test('remove class from elements', () => {
+
+    $('.foo').removeClass('remove-me');
+
+    expect(document.querySelectorAll('.remove-me').length).toBe(0);
+
+  });
+
+  test('toggle a class off & then back on', () => {
+
+    $('.foo').toggleClass('toggled');
+    expect(document.querySelector('.foo').classList.contains('toggled')).toBeTruthy();
+
+    $('.foo').toggleClass('toggled');
+    expect(document.querySelector('.foo').classList.contains('toggled')).toBeFalsy();
 
   });
 
